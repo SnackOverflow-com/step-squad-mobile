6f2514e585369fe1dddb2eb62adc07248a369bfe
mdbossss <ferrari020203@gmail.com>
2025-05-03 22:51:07 +0200

    remove component tests folder

--- components/__tests__/ThemedText-test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import renderer from "react-test-renderer";
-import { BaseText } from "../ui";
-
-it(`renders correctly`, () => {
-  const tree = renderer.create(<BaseText>Snapshot test!</BaseText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
